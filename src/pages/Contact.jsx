6fed408943214840fefcfa913bb452667a2a01e4
Cesia Cora <cesiacora@proton.me>
2024-04-02 23:39:33 -0300

    about me section changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,48 @@
 import { useTranslation } from 'react-i18next';
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 export const Contact = () => {
     const { t, i18n } = useTranslation();
+
+    const [email, setEmail] = useState("");
+    const [subject, setSubject] = useState("")
+    const [message, setMessage] = useState("");
+
+    const [submitted, setSubmitted] = useState(false);
+    const [error, setError] = useState("");
+
+    function submit(e) {
+        // This will prevent page refresh
+        e.preventDefault();
+
+        // replace this with your own unique endpoint URL
+        fetch("https://formcarry.com/s/XXXXXXX", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json"
+            },
+            body: JSON.stringify({ email: email, subject: subject, message: message })
+        })
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.code === 200) {
+                    setSubmitted(true);
+                } else {
+                    setError(res.message);
+                }
+            })
+            .catch((error) => setError(error));
+    }
+
+    if (error) {
+        return <p>{error}</p>;
+    }
+
+    if (submitted) {
+        return <p>We've received your message, thank you for contacting us!</p>;
+    }
 
     return (
         <>
@@ -13,13 +53,13 @@ export const Contact = () => {
             </div>
             <div id="contactContainer">
                 <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 1, delay: 0.8 }}>
-                    <form action="mailto:cesiacora@gmail.com" method="post" enctype="text/plain" className="form-control">
+                    <form onSubmit={submit} method="post" enctype="text/plain" className="form-control">
                         <label for="name">{t('contact.email')}</label>
-                        <input type="email" name="email" placeholder={t('contact.yourEmail')}></input>
+                        <input type="email" name="email" placeholder={t('contact.yourEmail')} value={email} onChange={(e) => setEmail(e.target.value)}></input>
                         <label for="subject">{t('contact.subject')}</label>
-                        <input type="text" name="subject" placeholder={t('contact.yourSubject')}></input>
+                        <input type="text" name="subject" placeholder={t('contact.yourSubject')} value={subject} onChange={(e) => setSubject(e.target.value)}></input>
                         <label for="message">{t('contact.message')}</label>
-                        <textarea placeholder={t('contact.yourMessage')}></textarea>
+                        <textarea placeholder={t('contact.yourMessage')} value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
                         <input type="submit" className="btn-link" value={t('contact.submit')}></input>
                     </form>
                 </motion.div>
